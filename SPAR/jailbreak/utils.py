452e55b024c974269dfffaaa3edd96616edbd832
Andy Arditi <andyrdt@gmail.com>
2023-10-25 18:17:50 +0000

    nbviewer gcg

--- SPAR/jailbreak/utils.py
+++ SPAR/jailbreak/utils.py
@@ -1,8 +1,6 @@
 import plotly.graph_objects as go
 import torch
 
-from IPython.display import clear_output
-
 def get_nonascii_nonspecial_toks(tokenizer, device='cpu'):
 
     def is_ascii(s):
@@ -33,16 +31,3 @@ def get_filtered_cand_toks(tokenizer, control_cand, curr_control=None):
             cand_toks.append(control_cand[i])
 
     return torch.stack(cand_toks)
-
-def update_plot(x_data, y_data, text_data):
-    clear_output(wait=True)
-    fig = go.Figure(
-        data=[go.Scatter(
-                x=x_data, y=y_data, text=text_data,
-                hoverinfo='x+y+text', mode='lines+markers', name='Loss'
-        )],
-        layout=go.Layout(
-            title='Loss', xaxis_title='Step', yaxis_title='Loss', width=600, height=400
-        )
-    )
-    fig.show()
