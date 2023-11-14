828f95908199ed4a96582798ddd19edc20203648
Andy Arditi <andyrdt@gmail.com>
2023-11-14 18:20:03 +0000

    fix prompt

--- SPAR/refusal_mi/utils.py
+++ SPAR/refusal_mi/utils.py
@@ -4,39 +4,52 @@ from jaxtyping import Int, Float
 from transformer_lens import HookedTransformer
 from transformers import AutoTokenizer
 
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+
 def instruction_to_prompt(
     instruction: str,
-    system_prompt: str="",
-    model_output: str="",
+    system_prompt: str=None,
+    model_output: str=None,
+    append_space: bool=True,
 ) -> str:
     """
     Converts an instruction to a prompt string structured for Llama2-chat.
-    Note that, unless model_output is supplied, the prompt will (intentionally) end with a space.
     See details of Llama2-chat prompt structure here: here https://huggingface.co/blog/llama2#how-to-prompt-llama-2
     """
-
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-
-    if len(system_prompt) == 0:
-        dialog_content = instruction.strip()
-    else:
+    if system_prompt is not None:
         dialog_content = B_SYS + system_prompt.strip() + E_SYS + instruction.strip()
-    prompt = f"{B_INST} {dialog_content}{E_INST} {model_output.strip()}"
+    else:
+        dialog_content = instruction.strip()
+
+    if model_output is not None:
+        prompt = f"{B_INST} {dialog_content.strip()} {E_INST} {model_output.strip()}"
+    else:
+        prompt = f"{B_INST} {dialog_content.strip()} {E_INST}"
+
+    if append_space:
+        prompt = prompt + " "
+
     return prompt
 
 def tokenize_instructions(
     tokenizer: AutoTokenizer,
     instructions: List[str],
     padding_length: int=None,
-    system_prompt: str="",
-    model_outputs: List[str]=None
+    system_prompt: str=None,
+    model_outputs: List[str]=None,
 ) -> Int[Tensor, "batch seq_len"]:
-    if model_outputs:
+    if model_outputs is not None:
         assert(len(instructions) == len(model_outputs))
-        prompts = [instruction_to_prompt(instruction, system_prompt, model_output) for (instruction, model_output) in zip(instructions, model_outputs)]
+        prompts = [
+            instruction_to_prompt(instruction, system_prompt, model_output, append_space=False)
+            for (instruction, model_output) in zip(instructions, model_outputs)
+        ]
     else:
-        prompts = [instruction_to_prompt(instruction, system_prompt) for instruction in instructions]
+        prompts = [
+            instruction_to_prompt(instruction, system_prompt, model_output=None, append_space=True)
+            for instruction in instructions
+        ]
 
     instructions_toks = tokenizer(
         prompts,
